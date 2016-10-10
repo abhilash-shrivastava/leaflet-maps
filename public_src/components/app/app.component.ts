65410215069c0520c63e100a64f005f9319c7dad
Abhilash Shrivastava <ashrivas@mail.sfsu.edu>
2016-10-10 16:37:13 -0700

    able to apply style properties on map component

--- public_src/components/app/app.component.ts
+++ public_src/components/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, ViewChild, Input, ElementRef} from '@angular/core';
 
 @Component({
     selector: 'app',
@@ -9,4 +9,16 @@ import {Component, ViewChild} from '@angular/core';
     providers: [],
 })
 export class AppComponent {
+    map: any;
+    lng: any ;
+    lat: any ;
+    zoom: any ;
+    style: any;
+    ngOnInit(){
+        this.map = (<HTMLScriptElement[]><any>document.getElementsByTagName('map'))[0];
+        this.lat = this.map.attributes.lat.nodeValue;
+        this.lng = this.map.attributes.lng.nodeValue;
+        this.zoom = this.map.attributes.zoom.nodeValue;
+        this.style = this.map.attributes.style.nodeValue;
+    }
 }
--- public_src/components/map/map.component.ts
+++ public_src/components/map/map.component.ts
@@ -1,7 +1,7 @@
 /**
  * Created by Abhi on 10/10/16.
  */
-import {Component, ViewChild} from '@angular/core'
+import {Component, ViewChild, Input} from '@angular/core'
 import {NavigatorComponent} from '../navigator/navigator.component';
 import {MarkerComponent} from '../marker/marker.component';
 import {MapService} from '../../services/map.service';
@@ -10,22 +10,16 @@ import {Location} from '../../core/location.class';
 
 @Component({
     selector: 'map',
-    template: '<div id="map"><navigator></navigator><marker></marker></div>',
-    styles: [`body {
-                margin:0;
-                padding:0;
-            }
-
-            #map {
-                position:absolute;
-                top:50px;
-                bottom:0;
-                width:100%;
-            }`
-    ]
+    template: '<div id="map"></div>',
+    styles: []
 })
 
 export class MapComponent{
+    @Input('lng') lng: any;
+    @Input('lat') lat: any;
+    @Input('zoom') zoom: any;
+    @Input('style') style: any;
+    map: any;
 
     @ViewChild(MarkerComponent) markerComponent:MarkerComponent;
 
@@ -33,10 +27,12 @@ export class MapComponent{
     }
 
     ngOnInit() {
+        this.map = <HTMLInputElement>document.getElementById('map');
+        this.map.style.cssText = this.style;
         var map = new L.Map('map', {
             zoomControl: false,
-            center: new L.LatLng(40.731253, -73.996139),
-            zoom: 12,
+            center: new L.LatLng(this.lat, this.lng),
+            zoom: this.zoom,
             minZoom: 4,
             maxZoom: 19,
             layers: [this.mapService.baseMaps.OpenStreetMap]
@@ -47,14 +43,14 @@ export class MapComponent{
         L.control.scale().addTo(map);
 
         this.mapService.map = map;
-        this.geocoder.getCurrentLocation()
-            .subscribe(
-                location => map.panTo([location.latitude, location.longitude]),
-                err => console.error(err)
-            );
+        // this.geocoder.getCurrentLocation()
+        //     .subscribe(
+        //         location => map.panTo([location.latitude, location.longitude]),
+        //         err => console.error(err)
+        //     );
     }
 
     ngAfterViewInit() {
-        this.markerComponent.Initialize();
+        // this.markerComponent.Initialize();
     }
 }
